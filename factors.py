af184b2158cb93d9066056ef34e3428edb9a3f7f
cbhallamudi <36947619+cbhallamudi@users.noreply.github.com>
2019-04-23 00:42:55 +0530

    Updated Factors.py
    Added the existing code to a function

--- factors.py
+++ factors.py
@@ -1,6 +1,8 @@
-number = eval(input("Get the number to get factors: "))
-dummyVariable = 2
-while dummyVariable < number:
-    if number%dummyVariable == 0:
-        print(dummyVariable,end=" ")
-    dummyVariable += 1        
+def getFactors():
+    number = eval(input("Get the number to get factors: "))
+    dummyVariable = 2
+    while dummyVariable < number:
+        if number%dummyVariable == 0:
+            print(dummyVariable,end=" ")
+        dummyVariable += 1
+getFactors()        
