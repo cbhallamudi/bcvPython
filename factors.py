61652472b955fc5b2b8e629efd9c3039cd8c32de
cbhallamudi <36947619+cbhallamudi@users.noreply.github.com>
2019-04-23 00:40:03 +0530

    Factors Program

---
+++ factors.py
@@ -0,0 +1,6 @@
+number = eval(input("Get the number to get factors: "))
+dummyVariable = 2
+while dummyVariable < number:
+    if number%dummyVariable == 0:
+        print(dummyVariable,end=" ")
+    dummyVariable += 1        
