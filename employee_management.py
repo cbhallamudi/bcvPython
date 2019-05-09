3b5fb68425ef52cf6cfb27c14f9fd64b7bd4238b
cbhallamudi <cbhallamudi@gmail.com>
2019-05-09 16:14:00 +0530

    Employee Management Write to file

--- employee_management.py
+++ employee_management.py
@@ -2,6 +2,7 @@ import packages.add_employee as ae
 import packages.get_employees as ge
 import packages.add_to_game as atg
 import packages.fetch_games_info as fgi
+import json
 emp_ids = {'e1','e2','e3'}
 game_ids = {'ch','ca'}
 emp_info = {
@@ -13,15 +14,31 @@ emp_games = {
     'chess':{'e1','e2'},
     'carroms':{'e3','e1'}
 }
-while True:
-    get_action_id = input("Press 'addemp' to add employee \n Press 'getemp' to get employee \n Press 'add2game' to add employee to game \n Press 'fetgame' to fetch game info \n any 't' to terminate \n Enter your choice:  ")
-    if get_action_id == 'addemp':
-        ae.add_employee(emp_ids,emp_info)
-    elif get_action_id == 'getemp':
-        ge.get_employee(emp_ids,emp_info)
-    elif get_action_id == 'add2game':
-        atg.emp_game_subscription(emp_ids,emp_info,game_ids,emp_games)    
-    elif get_action_id == 'fetgame':
-        fgi.fet_game_info(emp_ids,emp_info,game_ids,emp_games)
-    elif get_action_id == 't':
-        break
+
+emp_games_dict = {}
+for game in emp_games:
+    emp_games_dict[game] = list(emp_games[game])
+
+
+# while True:
+#     get_action_id = input("Press 'addemp' to add employee \n Press 'getemp' to get employee \n Press 'add2game' to add employee to game \n Press 'fetgame' to fetch game info \n any 't' to terminate \n Enter your choice:  ")
+#     if get_action_id == 'addemp':
+#         ae.add_employee(emp_ids,emp_info)
+#     elif get_action_id == 'getemp':
+#         ge.get_employee(emp_ids,emp_info)
+#     elif get_action_id == 'add2game':
+#         atg.emp_game_subscription(emp_ids,emp_info,game_ids,emp_games)    
+#     elif get_action_id == 'fetgame':
+#         fgi.fet_game_info(emp_ids,emp_info,game_ids,emp_games)
+#     elif get_action_id == 't':
+#         break
+
+main_json_dictionary = {
+    'emp_ids':list(emp_ids),
+    'game_ids':list(game_ids),
+    'emp_info':emp_info,
+    'emp_games':emp_games_dict
+}
+
+with open('employee_info.txt','w') as myfile:
+    json.dump(main_json_dictionary,myfile)
